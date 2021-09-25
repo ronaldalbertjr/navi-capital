daebf325e33544ed39495ba32e1fb93868ea8a33
ronaldalbertjr <ronaldalbert1609@gmail.com>
2021-09-25 16:37:30 -0300

    started streamlit

--- st_navi.py
+++ st_navi.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+from sklearn.linear_model import LinearRegression
 
 st.write("""
         # Hackaton Navi-Capital
@@ -6,14 +10,90 @@ st.write("""
         """
 )
 
+df_companies = pd.read_csv("datasets/companies_br.csv")
+df_companies_financials = pd.read_csv("datasets/companies_financials_br.csv")
+esg_scores = pd.read_csv("datasets/esg_scores_history_br.csv")
+global_esg = esg_scores[esg_scores.aspect.eq('S&P Global ESG Score')]
+df_companies = df_companies[df_companies.company_id.isin(global_esg.company_id.values)]
+df_companies_financials['ref_year'] = pd.to_datetime(df_companies_financials.ref_date).dt.year
+df_companies_financials['real_data_item_values'] = df_companies_financials['data_item_value'] * df_companies_financials.unit_value
+
 def select_company():
     company = st.sidebar.selectbox("Escolha uma empresa:",
-            ['Navi Capital', 'BITS Capital', 'Itau']
+            df_companies.company_name.values
             )
 
-    return company
+    return df_companies[df_companies.company_name.eq(company)].company_id.values[0] ,company
 
+def select_data_item(company_id):
+    data_item = st.sidebar.selectbox("Escolha um Indicador",
+            df_companies_financials[df_companies_financials.company_id.eq(company_id)].data_item.values
+            ) 
+    return data_item
 
-selected_company = select_company()
+def get_esg_score(company_id):
+    esg_score = global_esg[global_esg.company_id.eq(company_id)]
+    esg_score = esg_score[['assessment_year', 'score_value']]
+    return esg_score
+
+def generate_esg_graph(esg_df):
+    fig = go.Figure(data = go.Scatter(x=esg_df.assessment_year.values, y=esg_df.score_value.values))
+    fig.update_xaxes(nticks = len(esg_df.assessment_year.values))
+
+    return fig
+
+def get_financials_by_year(company_id, data_item):    
+    return_df =  df_companies_financials[df_companies_financials.company_id.eq(company_id) & df_companies_financials.data_item.eq(data_item)][['ref_year', 'real_data_item_values']]
+
+    return return_df    
+
+def generate_fin_graph(fin_df):
+    fig = go.Figure(data = go.Scatter(x=fin_df.ref_year.values, y=fin_df.real_data_item_values.values))
+    fig.update_xaxes(nticks= len(fin_df.ref_year.values))
+
+    return fig
+
+def generate_esg_fin_graph(esg_df, fin_df):
+    esg_fin_merged = pd.merge(esg_df, fin_df, how='inner', left_on='assessment_year', right_on='ref_year')
+
+    reg = LinearRegression().fit(esg_fin_merged['score_value'].values.reshape(-1, 1), esg_fin_merged['real_data_item_values'].values)
+
+    fig = go.Figure(data = go.Scatter(x=esg_fin_merged['score_value'].values, y=esg_fin_merged['real_data_item_values'].values, mode='markers'))
+
+    fig.add_trace(
+            go.Scatter(x=esg_fin_merged['score_value'].values,
+                y=reg.predict(esg_fin_merged['score_value'].values.reshape(-1, 1)),
+                mode='lines',
+                line=go.scatter.Line(color="gray"),
+                showlegend=False
+            )
+    )
+
+    fig.update_layout(
+                title="ESG Score X Indicador Financeiro"
+            )
+    return fig
+
+company_id, selected_company = select_company()
 
 st.write("""# {}""".format(selected_company))
+
+st.write("""# ESG Score""")
+esg_score = get_esg_score(company_id)
+
+esg_fig = generate_esg_graph(esg_score)
+
+st.write(esg_score)
+st.plotly_chart(esg_fig)
+
+selected_data_item = select_data_item(company_id)
+st.write("""# Indicador Financeiro: {} """.format(selected_data_item))
+
+financials_by_year = get_financials_by_year(company_id, selected_data_item)
+st.write(financials_by_year)
+
+fin_fig = generate_fin_graph(financials_by_year)
+st.plotly_chart(fin_fig)
+
+fin_esg_fig = generate_esg_fin_graph(esg_score, financials_by_year)
+st.plotly_chart(fin_esg_fig)
