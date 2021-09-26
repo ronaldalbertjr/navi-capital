86bbb419caf8c829771e4718be257b260b2dd2dd
Ronald Albert <ronaldalbert1609@gmail.com>
2021-09-25 21:18:42 -0300

    Update st_navi.py

--- st_navi.py
+++ st_navi.py
@@ -13,8 +13,9 @@ st.write("""
 df_companies = pd.read_csv("datasets/companies_br.csv")
 df_companies_financials = pd.read_csv("datasets/companies_financials_br.csv")
 esg_scores = pd.read_csv("datasets/esg_scores_history_br.csv")
-global_esg = esg_scores[esg_scores.aspect.eq('S&P Global ESG Score')]
-df_companies = df_companies[df_companies.company_id.isin(global_esg.company_id.values)]
+esg_scores = esg_scores[esg_scores.score_value != 0]
+esg_scores = esg_scores.dropna()
+df_companies = df_companies[df_companies.company_id.isin(esg_scores.company_id.unique())]
 df_companies_financials['ref_year'] = pd.to_datetime(df_companies_financials.ref_date).dt.year
 df_companies_financials['real_data_item_values'] = df_companies_financials['data_item_value'] * df_companies_financials.unit_value
 
@@ -31,8 +32,14 @@ def select_data_item(company_id):
             ) 
     return data_item
 
-def get_esg_score(company_id):
-    esg_score = global_esg[global_esg.company_id.eq(company_id)]
+def select_esg_score(company_id):
+    score = st.sidebar.selectbox("Escolha uma métrica do ESG",
+            esg_scores[esg_scores.company_id.eq(company_id)].aspect.values)
+
+    return score
+
+def get_esg_score(company_id, esg_aspect):
+    esg_score = esg_scores[esg_scores.company_id.eq(company_id) & esg_scores.aspect.eq(esg_aspect)]
     esg_score = esg_score[['assessment_year', 'score_value']]
     return esg_score
 
@@ -57,7 +64,7 @@ def generate_esg_fin_graph(esg_df, fin_df):
     esg_fin_merged = pd.merge(esg_df, fin_df, how='inner', left_on='assessment_year', right_on='ref_year')
 
     reg = LinearRegression().fit(esg_fin_merged['score_value'].values.reshape(-1, 1), esg_fin_merged['real_data_item_values'].values)
-
+    
     fig = go.Figure(data = go.Scatter(x=esg_fin_merged['score_value'].values, y=esg_fin_merged['real_data_item_values'].values, mode='markers'))
 
     fig.add_trace(
@@ -69,31 +76,31 @@ def generate_esg_fin_graph(esg_df, fin_df):
             )
     )
 
-    fig.update_layout(
-                title="ESG Score X Indicador Financeiro"
-            )
     return fig
 
 company_id, selected_company = select_company()
 
 st.write("""# {}""".format(selected_company))
 
-st.write("""# ESG Score""")
-esg_score = get_esg_score(company_id)
+selected_esg_score = select_esg_score(company_id)
+
+st.write("""# {}""".format(selected_esg_score))
+esg_score = get_esg_score(company_id, selected_esg_score)
 
 esg_fig = generate_esg_graph(esg_score)
 
-st.write(esg_score)
+st.write(esg_score.set_index('assessment_year'))
 st.plotly_chart(esg_fig)
 
 selected_data_item = select_data_item(company_id)
 st.write("""# Indicador Financeiro: {} """.format(selected_data_item))
 
 financials_by_year = get_financials_by_year(company_id, selected_data_item)
-st.write(financials_by_year)
+st.write(financials_by_year.set_index('ref_year'))
 
 fin_fig = generate_fin_graph(financials_by_year)
 st.plotly_chart(fin_fig)
 
+st.write("""# {}  X {}""".format(selected_esg_score, selected_data_item))
 fin_esg_fig = generate_esg_fin_graph(esg_score, financials_by_year)
 st.plotly_chart(fin_esg_fig)
