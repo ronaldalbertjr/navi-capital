513762129cde2738540b92ef4da5dc14ea11bd7b
ronaldalbertjr <ronaldalbert1609@gmail.com>
2021-09-26 22:02:01 -0300

    removing unnecessary scripts

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from multipage import MultiPage
 app = MultiPage()
 
 ## titulo principal
-st.title("Hackaton Navi-Capital")
+st.title("Navi Tech Journey")
 
-app.add_page("Hackaton Navi-Capital", principal.app)
+app.add_page("Navi Tech Journey", principal.app)
 app.add_page("ESG x métrica financeira", esg_fin.app)
 app.add_page("Análise Ambiental", ambiental.app)
 
